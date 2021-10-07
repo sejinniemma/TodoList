7c90c021f0d32dc46cd07cc0b6272e9eef7752e5
Emma <jsj0471@naver.com>
2021-10-07 19:28:39 +0900

    prettier🥰

--- main.js
+++ main.js
@@ -1,40 +1,40 @@
-'use strict';
+'use strict'
 
-const items = document.querySelector('.items');
-const addBtn = document.querySelector('.footer__button');
-const input = document.querySelector('.footer__input');
-const resetBtn = document.querySelector('.reset');
+const items = document.querySelector('.items')
+const addBtn = document.querySelector('.footer__button')
+const input = document.querySelector('.footer__input')
+const resetBtn = document.querySelector('.reset')
 
-addBtn.addEventListener('click',()=>{
-    onAdd();
+addBtn.addEventListener('click', () => {
+  onAdd()
 })
 
-input.addEventListener('keydown',(event)=>{
-    if(event.keyCode === 13){
-        onAdd();
-    }    
+input.addEventListener('keydown', (event) => {
+  if (event.keyCode === 13) {
+    onAdd()
+  }
 })
 
-function onAdd(){
-    const text = input.value;
-    if(input.value === ''){
-        input.value='';
-        return;
-    }
-    const item = creatItem(text);
-    items.appendChild(item);
-    item.scrollIntoView({block: 'center'});
-    input.value="";
-    input.focus();
+function onAdd() {
+  const text = input.value
+  if (input.value === '') {
+    input.value = ''
+    return
+  }
+  const item = creatItem(text)
+  items.appendChild(item)
+  item.scrollIntoView({ block: 'center' })
+  input.value = ''
+  input.focus()
 }
 
-let id = 0;
-function creatItem (text){
-    const itemRow = document.createElement('li');
-    itemRow.setAttribute('class','item__row');
-    itemRow.setAttribute('data-target-id', id)
+let id = 0
+function creatItem(text) {
+  const itemRow = document.createElement('li')
+  itemRow.setAttribute('class', 'item__row')
+  itemRow.setAttribute('data-target-id', id)
 
-    itemRow.innerHTML = ` 
+  itemRow.innerHTML = ` 
         <div class="item">
             <span class="item__name">${text}</span>
             <button class="item__delete">
@@ -42,19 +42,21 @@ function creatItem (text){
             </button>
         </div>
         <div class="item__divider"></div>
-  `;
-id++;
-  return itemRow;
+  `
+  id++
+  return itemRow
 }
 
-items.addEventListener('click',(event)=>{
-    const id = event.target.dataset.id;
-    if(id){
-        const toBeDeleted = document.querySelector(`.item__row[data-target-id="${id}"]`)
-        toBeDeleted.remove();
-    }
+items.addEventListener('click', (event) => {
+  const id = event.target.dataset.id
+  if (id) {
+    const toBeDeleted = document.querySelector(
+      `.item__row[data-target-id="${id}"]`,
+    )
+    toBeDeleted.remove()
+  }
 })
 
-resetBtn.addEventListener('click',()=>{
-    items.innerHTML = '';
+resetBtn.addEventListener('click', () => {
+  items.innerHTML = ''
 })
